731e46deaa94e4e45abe2527b6ed2a708fa5b620
sergethi <nsergearistide@gmail.com>
2022-04-27 21:08:36 +0000

    added basicAuth functionality to routes

--- app.js
+++ app.js
@@ -9,16 +9,45 @@ const saltRounds = 2;
 const {User, Item} = require('./models');
 const { use } = require("bcrypt/promises");
 
+
 // initialise Express
 const app = express();
 
 // specify out request bodies are json
 app.use(express.json());
 
-// routes go here
+//Home route
 app.get('/', (req, res) => {
   res.send('<h1>App Running</h1>')
 })
+
+//configure basicAuth
+app.use(basicAuth({
+  authorizer: dbAuthorizer,
+  authorizeAsync: true,
+  unauthorizedResponse: () => "you do not have access to this endpoint"
+}))
+
+
+//compare username and password with db users
+//return a boolean indicating a username and a pasword math
+async function dbAuthorizer(username, password,callback){
+  try{
+    //get matching user from db
+    const user = await User.findOne({where:{name: username}})
+    //if username is valid, match password
+    let isValid =  (user != null) ? await bcrypt.compare(password, user.password) : false;
+    console.log("username and password math?", true)
+    callback(null, isValid)
+  }
+  catch(err){
+    // if authorize fails, log error
+    console.log("erroe: ", err)
+    callback(null, false)
+  }
+}
+
+// routes go here
 
 //create users
 app.post('/users', async(req, res) => {
@@ -49,7 +78,9 @@ app.post('/session', async(req, res) => {
   else{
     bcrypt.compare(req.body.password, thisUser.password, async function(err, name){
       if(name){
-        res.send(`Logged as', ${thisUser.name}`)
+        //res.send(`Logged as, ${thisUser.name}`)
+        let user = thisUser.name
+        res.status(200).send({"loginAs":user})
       }
       else{
         res.send('password do not match')
@@ -76,6 +107,7 @@ app.get('/users/:id', async(req, res) => {
 //   let newUser = await User.create(req.body)
 //   res.json(newUser)
 // })
+
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
